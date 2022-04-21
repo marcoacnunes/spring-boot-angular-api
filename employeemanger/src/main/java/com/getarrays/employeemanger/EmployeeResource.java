0838186e98beeb3bc08f907b26cd3c57c521b15f
Marco <marco.acnunes369@gmail.com>
2022-04-21 23:23:09 +0000

    c

--- employeemanger/src/main/java/com/getarrays/employeemanger/EmployeeResource.java
+++ employeemanger/src/main/java/com/getarrays/employeemanger/EmployeeResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class EmployeeResource {
 		this.employeeService = employeeService;
 	}
 	
-	@GetMapping
+	@GetMapping("/all")
 	public ResponseEntity<List<Employee>> getAllEmployee() {
 		List<Employee> employees = employeeService.findAllEmployees();
 		return new ResponseEntity<>(employees, HttpStatus.OK);
@@ -32,13 +33,19 @@ public class EmployeeResource {
 
 	@GetMapping("/find/{id}") 
 	public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
-		Employee employee = employeeService.findEmployeeById(id);
-		return new ResponseEntity<>(employee, HttpStatus.OK);
+		Employee newEmployee = employeeService.findEmployeeById(id);
+		return new ResponseEntity<>(newEmployee, HttpStatus.OK);
 	}
 	
 	@PostMapping ("/add")
-	public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+	public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
 		Employee newEmployee = employeeService.addEmployee(employee);
+		return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+	}
+	
+	@PutMapping ("/update")
+	public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+		Employee newEmployee = employeeService.updateEmployee(employee);
 		return new ResponseEntity<>(newEmployee, HttpStatus.OK);
 	}
 	
